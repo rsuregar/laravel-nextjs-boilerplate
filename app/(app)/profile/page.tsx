8513f43c12957b1e1826c99bde35db0107cc743d
Muhammad Rahmat <muhammad.rahmat@wartek.belajar.id>
2024-10-30 19:02:31 +0800

    fix: profil page

--- app/(app)/profile/page.tsx
+++ app/(app)/profile/page.tsx
@@ -1,12 +1,9 @@
 import { User } from "@/types"
-import { FC } from "react"
 
 interface IProfileProps {
   user?: User
 }
 
-const Profile: FC<IProfileProps> = ({ user }: IProfileProps) => {
+export default function Profile({ user }: IProfileProps) {
   return <div>halo {JSON.stringify(user)}</div>
 }
-
-export default Profile
